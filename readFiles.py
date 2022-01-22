56cc41158e6e198d8566b47431f1620f4ca97c68
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-22 06:02:51 +0300

    Write a readFile function to ensure the test passes

--- readFiles.py
+++ readFiles.py
@@ -4,3 +4,14 @@
 #   print(data)
 
 
+textFile = 'test.txt'
+
+def readFile(textFile) :
+  '''
+  Function that reads a text file and returns the data from the text file
+  '''
+
+  with open(textFile, 'r') as handle :
+    data = handle.read()
+
+    return data
