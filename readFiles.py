d702d2ca9592f1af6cfa59a0e5b1908bf6618725
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-22 06:18:40 +0300

    Nest the with-operator inside a try-block and catch the FileNotFoundError and with an exception we return None

--- readFiles.py
+++ readFiles.py
@@ -15,3 +15,22 @@ def readFile(textFile) :
     data = handle.read()
 
     return data
+
+
+
+def readFile(textFile) :
+  '''
+  Function that reads the text file and returns the data from the text file
+  
+  Raises:
+    FileNotFoundError: If it cannot find the file
+  '''
+
+  try :
+    with open(textFile, 'r') as handle :
+      data = handle.read()
+
+      return data
+
+  except FileNotFoundError :
+    return None
